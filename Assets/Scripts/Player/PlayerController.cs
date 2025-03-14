594438996cf467225c4bb9aa692647124b92b517
Cerunoo <145900005+Cerunoo@users.noreply.github.com>
2025-03-15 03:46:55 +0700

    Update

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerController : MonoBehaviour, IPlayerController
+{
+    public static PlayerController Instance { get; private set; }
+
+    [SerializeField] private ScriptableStats _stats;
+
+    [Header("States")]
+    public bool facingRight;
+    public bool isMild;
+
+    private FrameInput _frameInput;
+
+    private Rigidbody2D _rb;
+
+    private Vector2 _frameVelocity;
+
+    #region Interface
+
+    public Vector2 FrameDirection => new Vector2(_frameInput.Horizontal, _frameVelocity.y);
+    public bool IsMild => isMild;
+
+    #endregion
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GatherInput();
+    }
+
+    private void GatherInput()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        _frameInput = new FrameInput
+        {
+            Horizontal = inputHorizontal != 0 ? inputHorizontal : Mathf.Lerp(_frameInput.Horizontal, inputHorizontal, Time.fixedDeltaTime * 10f),
+        };
+
+        if (_stats.SnapInput)
+        _frameInput.Horizontal = Mathf.Abs(_frameInput.Horizontal) < _stats.HorizontalDeadZoneThreshold ? 0 : _frameInput.Horizontal;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleDirection();
+
+        ApplyMovement();
+    }
+
+    #region Horizontal
+
+    private void HandleDirection()
+    {
+        if (_frameInput.Horizontal == 0)
+        {
+            var deceleration = _stats.GroundDeceleration;
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            float speed = !isMild ? _stats.NormalSpeed : _stats.MildSpeed;
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Horizontal * speed, _stats.Acceleration * Time.fixedDeltaTime);
+        }
+
+        if (_frameInput.Horizontal > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (_frameInput.Horizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        facingRight = !facingRight;
+    }
+
+    #endregion
+
+    private void ApplyMovement() => _rb.linearVelocity = _frameVelocity;
+}
+
+public struct FrameInput
+{
+    public float Horizontal;
+}
+
+public interface IPlayerController
+{
+    public Vector2 FrameDirection { get; }
+    public bool IsMild { get; }
+}
---
+++ Assets/Scripts/Player/ScriptableStats.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptableStats : ScriptableObject
+{
+    [Header("INPUT")]
+    [Tooltip("Makes all Input snap to an integer. Prevents gamepads from walking slowly. Recommended value is true to ensure gamepad/keybaord parity.")]
+    public bool SnapInput = true;
+
+    [Tooltip("Minimum input required before a left or right is recognized. Avoids drifting with sticky controllers"), Range(0.01f, 0.99f)]
+    public float HorizontalDeadZoneThreshold = 0.1f;
+
+    [Header("MOVEMENT")]
+    [Tooltip("Maximum normal speed")]
+    public float NormalSpeed = 14;
+
+    [Tooltip("Maximum mild speed")]
+    public float MildSpeed = 10;
+
+    [Tooltip("The player's capacity to gain horizontal speed")]
+    public float Acceleration = 120;
+
+    [Tooltip("The pace at which the player comes to a stop")]
+    public float GroundDeceleration = 60;
+}
